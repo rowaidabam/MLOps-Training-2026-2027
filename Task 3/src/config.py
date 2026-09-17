587ade2510fb22c59f75150bb66adb3eae3232e7
Rowaida <rowaidabam@gmail.com>
2026-09-18 00:17:02 +0300

    Add pre-commit hooks, fix trailing whitespace and EOF formatting

--- Task 3/src/config.py
+++ Task 3/src/config.py
@@ -1,6 +1,13 @@
+import os
 from pathlib import Path
 import yaml
 
+
 def load_config(config_path="config/config.yaml"):
     with open(config_path) as f:
-        return yaml.safe_load(f)
+        config = yaml.safe_load(f)
+
+    if "MLFLOW_TRACKING_URI" in os.environ:
+        config["mlflow"]["tracking_uri"] = os.environ["MLFLOW_TRACKING_URI"]
+
+    return config
